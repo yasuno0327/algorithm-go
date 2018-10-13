311fa30852a4acd0524ae0cb97164e68baf94bc6
yasuno0327 <5315076@ed.tus.ac.jp>
2018-10-14 02:00:04 +0900

    projectA

---
+++ beginners-selection/projectA.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	i := ReadNewInt()
+	j, k := ReadNewIntDelimitedSpace()
+	total := i + j + k
+	s := ReadNewLine()
+	fmt.Printf("%d %s\n", total, s)
+}
+
+func ReadNewInt() int {
+	var sc = bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	text := sc.Text()
+	i, err := strconv.Atoi(text)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func ReadNewIntDelimitedSpace() (int, int) {
+	var sc = bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	text1 := sc.Text()
+	sc.Scan()
+	text2 := sc.Text()
+	j, _ := strconv.Atoi(text1)
+	k, _ := strconv.Atoi(text2)
+	return j, k
+}
+
+func ReadNewLine() string {
+	var sc = bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	return sc.Text()
+}
